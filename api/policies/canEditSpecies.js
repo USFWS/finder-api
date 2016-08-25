9dd0a4ee7534807df384afce255f20c806529a38
Roy Hewitt <roy_hewitt@fws.gov>
2016-08-25 08:25:57 -0400

    Generalize policy to canEdit

--- api/policies/canEditSpecies.js
+++
@@ -1,6 +0,0 @@
-module.exports = function(req, res, next) {
-  if (req.user[0].accountType === 'viewer') {
-    return res.send(403, { message: 'You do not have editing privileges.' });
-  }
-  next();
-};
