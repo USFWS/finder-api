8b6a789dcf0362a35671087ea39e68cef965f4d8
Roy Hewitt <roy_hewitt@fws.gov>
2016-08-25 08:27:37 -0400

    Add link to FR document outlining categories

--- api/controllers/CategoriesController.js
+++ api/controllers/CategoriesController.js
@@ -1,7 +1,9 @@
 /**
  * CategoriesController
  *
- * @description :: Server-side logic for managing species categorization
+ * @description :: Server-side logic for managing species categorization.
+ *  See Federal Register for details on categories:
+ *  https://www.federalregister.gov/articles/2016/01/15/2016-00616/draft-methodology-for-prioritizing-status-reviews-and-accompanying-12-month-findings-on-petitions
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
