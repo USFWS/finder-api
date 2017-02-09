abb3676bf3b001df347b51f661c1e2ab6a0b5040
Roy Hewitt <roy_hewitt@fws.gov>
2017-02-09 12:25:19 -0500

    Create blank controller to take advantage of Blueprint API

---
+++ api/controllers/LandsController.js
@@ -0,0 +1,10 @@
+/**
+ * LandsController
+ *
+ * @description :: Server-side logic for managing protected lands
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+
+};
---
+++ api/controllers/SpeciesLandsController.js
@@ -0,0 +1,11 @@
+/**
+ * SpeciesLandsController
+ *
+ * @description :: Server-side logic for managing the join table linking species
+ *  with protected lands.
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+
+};
