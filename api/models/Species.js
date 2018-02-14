a58e6c858da920dd93e8f68ff7d4672b2b8ade76
Roy Hewitt <roy_hewitt@fws.gov>
2018-02-14 08:47:11 -0500

    Support SSA link

--- api/models/Species.js
+++ api/models/Species.js
@@ -1,95 +1,96 @@
 /**
-* Species.js
-*
-* @description :: TODO: You might write a short summary of how this model works and what it represents here.
-* @docs        :: http://sailsjs.org/#!documentation/models
-*/
+ * Species.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/#!documentation/models
+ */
 
 module.exports = {
-
   attributes: {
-
     scientificName: {
-      type: 'string',
+      type: "string",
       required: true,
       unique: true
     },
 
     commonName: {
-      type: 'string'
+      type: "string"
     },
 
     taxon: {
-      type: 'string',
+      type: "string",
       required: true,
       enum: [
-        'Amphibian',
-        'Amphipod',
-        'Bee',
-        'Beetle',
-        'Bird',
-        'Butterfly',
-        'Caddisfly',
-        'Crayfish',
-        'Crustacean',
-        'Dragonfly',
-        'Fly',
-        'Fish',
-        'Isopod',
-        'Mammal',
-        'Moth',
-        'Mussel',
-        'Non-Vascular Plant',
-        'Reptile',
-        'Snail',
-        'Stonefly',
-        'Vascular Plant'
+        "Amphibian",
+        "Amphipod",
+        "Bee",
+        "Beetle",
+        "Bird",
+        "Butterfly",
+        "Caddisfly",
+        "Crayfish",
+        "Crustacean",
+        "Dragonfly",
+        "Fly",
+        "Fish",
+        "Isopod",
+        "Mammal",
+        "Moth",
+        "Mussel",
+        "Non-Vascular Plant",
+        "Reptile",
+        "Snail",
+        "Stonefly",
+        "Vascular Plant"
       ]
     },
 
     leadOffice: {
-      type: 'string'
+      type: "string"
     },
 
     range: {
-      type: 'array'
+      type: "array"
     },
 
     status: {
-      type: 'array'
+      type: "array"
     },
 
     proposedDetermination: {
-      type: 'string'
+      type: "string"
+    },
+
+    ssa: {
+      type: "string"
     },
 
     categorization: {
-      type: 'array'
+      type: "array"
     },
 
     // Four digit species code in ECOS
     ecos: {
-      type: 'string'
+      type: "string"
     },
 
     // A species can be associated with one or more offices
     offices: {
-      collection: 'offices',
-      via: 'species'
+      collection: "offices",
+      via: "species"
     },
 
     // A species can be associated with one or more users (lead biologist)
     experts: {
-      collection: 'user',
-      via: 'species'
+      collection: "user",
+      via: "species"
     },
 
     // A species can be associated with one or more public lands
     lands: {
-      collection: 'lands',
-      via: 'land',
-      through: 'specieslands'
+      collection: "lands",
+      via: "land",
+      through: "specieslands"
     }
-
   }
 };
