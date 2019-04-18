3b413183740b18f9a5f0936da40e3a7ebf9bc911
Roy Hewitt <roy_hewitt@fws.gov>
2019-04-18 08:36:58 -0400

    Differentiate between range editor and editor

--- api/policies/canEdit.js
+++ api/policies/canEdit.js
@@ -1,6 +1,10 @@
 module.exports = function(req, res, next) {
-  if (req.user[0].accountType === 'viewer') {
+  var accountType = req.user[0].accountType;
+  if (['editor', 'admin'].includes(accountType)) {
+    next();
+  } else if (accountType === 'range editor') {
+    return res.send(403, { message: 'You only have permission to update a species’ range.' });
+  } else {
     return res.send(403, { message: 'You do not have editing privileges.' });
   }
-  next();
 };
